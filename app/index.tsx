9b9082e209a5831196b637c4b4314e7fda4a691e
Riefqi <riefqikamal369@gmail.com>
2024-11-09 18:07:31 +0800

    Add input and show output.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,51 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { FlatList, Text, TextInput } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const [text, setText] = useState("");
+  const [items, setItems] = useState<string[]>([]);
+
+  const handleList = () => {
+    setItems([...items, text]);
+    setText("");
+  };
+
   return (
-    <View
+    <SafeAreaView
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        padding: 10,
+        backgroundColor: "white",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <TextInput
+        style={{
+          fontSize: 20,
+          borderWidth: 1,
+          height: 50,
+          paddingHorizontal: 5,
+        }}
+        onSubmitEditing={handleList}
+        returnKeyType="done"
+        onChangeText={setText}
+        value={text}
+        placeholder="Type something..."
+        placeholderTextColor="#000"
+      />
+
+      <FlatList
+        data={items}
+        keyExtractor={(index) => index.toString()}
+        renderItem={({ item }) => (
+          <Text style={{ fontSize: 20, color: "black" }}>{item}</Text>
+        )}
+        style={{
+          padding: 5,
+          borderWidth: 1,
+          marginVertical: 10,
+        }}
+      />
+    </SafeAreaView>
   );
 }
